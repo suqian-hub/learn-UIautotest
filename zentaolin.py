ff322d40f72f7ec170d4389b2da5300112cacf51
suqian-hub <54240470+suqian-hub@users.noreply.github.com>
2020-08-29 23:26:26 +0800

    Update zentaolin.py

--- zentaolin.py
+++ zentaolin.py
@@ -13,7 +13,7 @@ Zentao-v12.0.1-64bit
 '''
 #打开网页
 browser.get("http://127.0.0.1/zentao/user-login-L3plbnRhby8=.html")
-time.sleep(2)
+time.sleep(1)
 #用户名
 browser.find_element_by_xpath("//*[@id='account']").send_keys("admin")
 #密码
@@ -24,23 +24,22 @@ browser.find_element_by_xpath('//*[@id="submit"]').click()
 #定位标题
 browser.refresh()
 tilte = browser.find_element_by_css_selector("body.m-my-index:nth-child(2) div:nth-child(1) div.container hgroup:nth-child(1) h1:nth-child(1) > a:nth-child(1)")
-time.sleep(1)
+time.sleep(0.5)
 # 断言
 if "dotest" in tilte.text:
     print("登录成功")
 else:
     print("登录失败")
-time.sleep(1)
 
 #测试模块
 browser.find_element_by_xpath("//nav[@id='navbar']//li[4]//a[1]").click()
-time.sleep(1)
+time.sleep(0.5)
 #Bug
 browser.find_element_by_xpath("//ul[@class='nav nav-default']//a[contains(text(),'Bug')]").click()
 #点击提交bug
 #browser.refresh()
 browser.find_element_by_xpath("//a[@class='btn btn-primary']").click()
-time.sleep(1)
+time.sleep(0.5)
 
 '''
 select标签定位(旧版禅道v9.4）
@@ -65,7 +64,7 @@ Select(module).select_by_index(1) #选择下拉字典项的操作
 '''
 #所属模块（新版使用li隐藏标签）
 browser.find_element_by_xpath("//div[@id='module_chosen']//a[@class='chosen-single']").click()
-time.sleep(1)
+time.sleep(0.5)
 ele1 = browser.find_element_by_xpath("//div[@class='chosen-drop chosen-auto-max-width chosen-no-wrap in']")
 #ActionChains(browser).move_to_element(ele1).perform()
 ele2 = browser.find_element_by_xpath("//div[@id='moduleIdBox']//li[2]")
@@ -74,55 +73,54 @@ ele2.click()
 
 #所属项目
 browser.find_element_by_xpath("//div[@id='project_chosen']").click()
-time.sleep(1)
+time.sleep(0.5)
 ele3 = browser.find_element_by_xpath("/html[1]/body[1]/main[1]/div[1]/div[1]/div[1]/form[1]/table[1]/tbody[1]/tr[2]/td[1]/span[1]/div[1]/div[1]/ul[1]/li[1]") 
 ActionChains(browser).move_to_element(ele3).perform()
 ele3.click()
 
 #影响版本
 browser.find_element_by_xpath("//div[@id='openedBuild_chosen']").click()
-time.sleep(1)
+time.sleep(0.5)
 ele4 = browser.find_element_by_xpath("/html[1]/body[1]/main[1]/div[1]/div[1]/div[1]/form[1]/table[1]/tbody[1]/tr[2]/td[2]/div[1]/div[1]/div[1]")
 ele5 = browser.find_element_by_xpath("/html[1]/body[1]/main[1]/div[1]/div[1]/div[1]/form[1]/table[1]/tbody[1]/tr[2]/td[2]/div[1]/div[1]/div[1]/ul[1]/li[3]")
 ActionChains(browser).move_to_element(ele4).move_to_element(ele5).perform()
 ele5.click()
-time.sleep(1)
+time.sleep(0.5)
 
 #当前指派
 browser.find_element_by_xpath("//div[@id='assignedTo_chosen']").click()
-time.sleep(1)
+time.sleep(0.5)
 ele6 = browser.find_element_by_xpath("/html[1]/body[1]/main[1]/div[1]/div[1]/div[1]/form[1]/table[1]/tbody[1]/tr[3]/td[1]/div[1]/div[1]/div[1]/ul[1]")
 ele7 = browser.find_element_by_xpath("/html[1]/body[1]/main[1]/div[1]/div[1]/div[1]/form[1]/table[1]/tbody[1]/tr[3]/td[1]/div[1]/div[1]/div[1]/ul[1]/li[3]")
 ActionChains(browser).move_to_element(ele6).move_to_element(ele7).perform()
 ele7.click()
-time.sleep(1)
+time.sleep(0.5)
 
 #Bug标题
 browser.find_element_by_xpath("//div[@class='input-control has-icon-right required']//input[@id='title']").send_keys("UI自动化测试，提交bug！")
 
 #严重程度:1
 browser.find_element_by_xpath("/html[1]/body[1]/main[1]/div[1]/div[1]/div[1]/form[1]/table[1]/tbody[1]/tr[5]/td[1]/div[1]/div[2]/button[1]").click()
-time.sleep(1)
+time.sleep(0.5)
 ele8 = browser.find_element_by_xpath("/html[1]/body[1]/main[1]/div[1]/div[1]/div[1]/form[1]/table[1]/tbody[1]/tr[5]/td[1]/div[1]/div[2]/div[1]/div[1]")
 ele9 = browser.find_element_by_xpath("/html[1]/body[1]/main[1]/div[1]/div[1]/div[1]/form[1]/table[1]/tbody[1]/tr[5]/td[1]/div[1]/div[2]/div[1]/div[1]/span[1]")
 ActionChains(browser).move_to_element(ele8).move_to_element(ele9).perform()
 ele9.click()
-time.sleep(1)
+time.sleep(0.5)
 
 #优先级:1
 browser.find_element_by_xpath("/html[1]/body[1]/main[1]/div[1]/div[1]/div[1]/form[1]/table[1]/tbody[1]/tr[5]/td[1]/div[1]/div[3]/button[1]").click()
-time.sleep(1)
+time.sleep(0.5)
 ele10 = browser.find_element_by_xpath("/html[1]/body[1]/main[1]/div[1]/div[1]/div[1]/form[1]/table[1]/tbody[1]/tr[5]/td[1]/div[1]/div[3]/div[1]/div[1]")
 ele11 = browser.find_element_by_xpath("/html[1]/body[1]/main[1]/div[1]/div[1]/div[1]/form[1]/table[1]/tbody[1]/tr[5]/td[1]/div[1]/div[3]/div[1]/div[1]/span[1]")
 ActionChains(browser).move_to_element(ele10).move_to_element(ele11).perform()
 ele11.click()
-time.sleep(1)
+time.sleep(0.5)
 
 
 '''
 重新步骤
 因为标签含有iframe；所以需要用其他的方法进行
-
 多层框架或窗口
 frame标签：frame标签有frameset、frame、iframe三种；frame、iframe需要切换，否则无法定位到；
 1.iFrame有ID 或者 name的情况
@@ -132,14 +130,19 @@ driver.switch_to.frame("id或者name")
 //跳出frame,进入default content;
 dr.switchTo().defaultContent();
 '''
+
 #重现步骤
-time.sleep(5)
-frameid = browser.find_element_by_xpath('//*[@id="dataform"]/table/tbody/tr[6]/td/div[2]/div[2]/iframe') #使用xpath定位到iframe标签
-browser.switch_to.frame(frameid) #dr.switch_to.frame切换至iframe标签；如果有id或name的话直接：driver.switch_to.frame('id')
+frameid = browser.find_element_by_xpath("//iframe[@class='ke-edit-iframe']")
+browser.switch_to.frame(frameid)
 browser.find_element_by_xpath('/html/body').clear()  #清空；对元素的操作
-browser.find_element_by_xpath('/html/body').send_keys("这是内容")
-#跳出iframe标签
+browser.find_element_by_xpath('/html/body').send_keys("登录禅道测试账号，提交bug。")
 browser.switch_to.default_content()
+time.sleep(0.5)
+
+#点击保存功能
+browser.find_element_by_xpath("//button[@id='submit']").click()  
+
+#browser.quit() #退出浏览器
 
 
 '''
@@ -150,11 +153,8 @@ driver.execute_script('window.scrollTo(0,2500);')
 
 driver.execute_script('window.scrollTo(2500,0);')
 横向滑动：2500,0；0：左；2500：右（2500-3000）
-'''
 browser.execute_script('window.scrollTo(0,2500);')   #滑动到底部
 time.sleep(5)
-
-
 '''
 #上传附件：input类型
 
