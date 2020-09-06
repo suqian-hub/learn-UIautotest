541eb6d83654023ba474802ec1488dfd5e225b2f
suqian-hub <54240470+suqian-hub@users.noreply.github.com>
2020-09-06 23:59:21 +0800

    Add files via upload

---
+++ object_build.py
@@ -0,0 +1,19 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.action_chains import ActionChains
+
+import unittest
+
+class Object_Build_TestCase(unittest.TestCase):
+
+    def setUp(self):
+        driver = 'C:\chromedriver_win32\chromedriver.exe'
+        self.browser = webdriver.Chrome(executable_path = driver)
+        self.browser.implicitly_wait(10)
+
+    def tearDown(self):
+        pass
+        #browser.quit() #退出浏览器
+
+    def test_case1(self):
