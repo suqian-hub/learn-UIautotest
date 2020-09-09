ea1eaa82ef120954ab25e39d46f55f5b475c3eb6
suqian-hub <54240470+suqian-hub@users.noreply.github.com>
2020-09-09 23:27:12 +0800

    Update test_task.py

--- test_task.py
+++ test_task.py
@@ -17,3 +17,17 @@ class Test_Task_TestCase(unittest.TestCase):
         #browser.quit() #退出浏览器
 
     def test_case1(self):
+        
+        self.browser.get("http://127.0.0.1/zentao/user-login-L3plbnRhby8=.html")
+        time.sleep(0.5)
+        
+        self.browser.find_element_by_xpath("//*[@id='account']").send_keys("admin")
+        self.browser.find_element_by_xpath("//input[@name='password']").send_keys("Aa123456")
+        self.browser.find_element_by_xpath('//*[@id="submit"]').click()
+
+        time.sleep(0.2)
+        #self.browser.refresh()
+        title = self.browser.title
+        #print(title)
+        self.assertEqual(title, "我的地盘 - 禅道")
+        print("登陆成功")
